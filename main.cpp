7c22fa2fa5c393c030e0aef2f3129ff34388a5b7
Haripriya129 <69579424+Haripriya129@users.noreply.github.com>
2021-05-18 15:29:34 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,count=0;
+    cin>>n;
+    while(1)//(n1=0)
+    {
+        if(n&1==1)
+        {
+            count++;
+        }
+        n=n>>1;
+      /*  if(n==0)
+        {
+            break;
+        }*/
+    }
+    cout<<count;
+    return 0;
+}
